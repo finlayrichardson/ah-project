5a4d0b4263be4f9717ae9e8648b8408aa2a3597e
0xfinlay <finlay.richardson1@gmail.com>
2022-01-16 00:08:40 +0000

    Move old

--- old/hello.py
+++
@@ -1,9 +0,0 @@
-percentage = int(input("Enter the percentage: "))
-if percentage > 70:
-    print("Grade is A")
-elif percentage > 60:
-    print("Grade is B")
-elif percentage > 50:
-    print("Grade is C")
-else:
-    print("Grade is F")
